3e6c3be25a36a7cbd48e084e2104e5cd6978b67e
John Fuquene <jfuquene@email.wm.edu>
2021-06-03 20:32:08 -0400

    render game function

--- index.js
+++ index.js
@@ -27,3 +27,22 @@ function getRandomCard() {
         return randomNumber
     }
 }
+
+function renderGame() {
+    cardsEl.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
+    
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageEl.textContent = message
+}
